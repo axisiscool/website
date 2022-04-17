cbbc3f5f87c11dfe61bd5af2f1d221ffa20cb156
Jonathan <54381371+axisiscool@users.noreply.github.com>
2022-04-17 02:37:34 -0400

    feat: improve colors and responsiveness

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,21 +5,21 @@ const Index: NextPage = () => {
   return (
     <div className="flex h-screen items-center justify-center bg-cream text-center dark:bg-slate-900">
       <div className="text-center">
-        <h1 className="text-4xl font-semibold dark:text-white lg:text-5xl">Hi, I'm Jonathan.</h1>
-        <p className=" pt-3 text-lg dark:text-white lg:text-xl">A Full-Stack Developer from New York.</p>
+        <h1 className="text-4xl font-semibold dark:text-white md:text-5xl">Hi, I'm Jonathan.</h1>
+        <p className="pt-3 text-lg dark:text-white md:text-xl">A Full-Stack Developer from New York.</p>
         <div className="grid grid-cols-3 gap-2 pt-5 lg:gap-3">
           <Link href="/projects">
-            <button className="rounded-lg bg-green-400 p-2 pl-14 pr-14 text-center text-lg font-bold text-slate-800 shadow-md hover:bg-green-500 dark:bg-green-100 dark:shadow-lg dark:hover:bg-green-200">
+            <button className="rounded-lg bg-blue-400 p-2 px-6 md:px-8 lg:px-10 text-center text-lg font-bold text-slate-800 shadow-md hover:bg-blue-500 dark:bg-blue-300 dark:shadow-lg dark:hover:bg-blue-400">
               Projects
             </button>
           </Link>
           <Link href="https://github.com/axisiscool">
-            <button className="pl-15 pr-15 rounded-lg bg-green-400 p-2 text-center text-lg font-bold text-slate-800 shadow-md hover:bg-green-500 dark:bg-green-100 dark:shadow-lg dark:hover:bg-green-200">
+            <button className="px-6 rounded-lg bg-blue-400 p-2 md:px-8 lg:px-10 text-center text-lg font-bold text-slate-800 shadow-md hover:bg-blue-500 dark:bg-blue-300 dark:shadow-lg dark:hover:bg-blue-400">
               Github
             </button>
           </Link>
           <Link href="https://discord.com/users/461750180388274180">
-            <button className="pl-15 pr-15 rounded-lg bg-green-400 p-2 text-center text-lg font-bold text-slate-800 shadow-md hover:bg-green-500 dark:bg-green-100 dark:shadow-lg dark:hover:bg-green-200">
+            <button className="px-6 md:px-8 lg:px-10 rounded-lg bg-blue-400 p-2 text-center text-lg font-bold text-slate-800 shadow-md hover:bg-blue-500 dark:bg-blue-300 dark:shadow-lg dark:hover:bg-blue-400">
               Discord
             </button>
           </Link>
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -7,7 +7,7 @@ const Projects: NextPage = () => {
       <div className="text-center">
         <h1 className="text-5xl font-semibold dark:text-white">Projects</h1>
         <div className="grid grid-cols-1 grid-rows-3 gap-4 pt-8 md:grid-cols-2 lg:grid-cols-3">
-          <div className="max-w-sm rounded-lg bg-green-400 p-2 pl-14 pr-14 text-center text-lg font-bold text-slate-800 shadow-md hover:bg-green-500 dark:bg-green-100 dark:shadow-lg dark:hover:bg-green-200">
+          <div className="max-w-sm rounded-lg bg-blue-400 p-2 px-10 text-center text-lg font-bold text-slate-800 shadow-md hover:bg-blue-500 dark:bg-blue-300 dark:shadow-lg dark:hover:bg-blue-400">
             <u>
               <Link href="https://github.com/Parallel-Development">Parallel</Link>
             </u>
@@ -16,13 +16,13 @@ const Projects: NextPage = () => {
               servers.
             </p>
           </div>
-          <div className="max-w-sm rounded-lg bg-green-400 p-2 px-10 text-center text-lg font-bold text-slate-800 shadow-md hover:bg-green-500 dark:bg-green-100 dark:shadow-lg dark:hover:bg-green-200">
+          <div className="max-w-sm rounded-lg bg-blue-400 p-2 px-10 text-center text-lg font-bold text-slate-800 shadow-md hover:bg-blue-500 dark:bg-blue-300 dark:shadow-lg dark:hover:bg-blue-400">
             <u>
               <Link href="https://github.com/axisiscool/velocity">Velocity</Link>
             </u>
             <p className="text-sm">A super fast API Wrapper for Perspective.</p>
           </div>
-          <div className="max-w-sm rounded-lg bg-green-400 p-2 px-10 text-center text-lg font-bold text-slate-800 shadow-md hover:bg-green-500 dark:bg-green-100 dark:shadow-lg dark:hover:bg-green-200">
+          <div className="max-w-sm rounded-lg bg-blue-400 p-2 px-10 text-center text-lg font-bold text-slate-800 shadow-md hover:bg-blue-500 dark:bg-blue-300 dark:shadow-lg dark:hover:bg-blue-400">
             <u>
               <Link href="https://github.com/SoundDrout-Dev">Sound's Utilities</Link>
             </u>
