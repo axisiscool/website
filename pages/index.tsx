d4b80071321d3aa1336453caa60af555387df3b6
Jonathan <54381371+axisiscool@users.noreply.github.com>
2022-04-17 02:41:19 -0400

    chore: prettier run

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,17 +9,17 @@ const Index: NextPage = () => {
         <p className="pt-3 text-lg dark:text-white md:text-xl">A Full-Stack Developer from New York.</p>
         <div className="grid grid-cols-3 gap-2 pt-5 lg:gap-3">
           <Link href="/projects">
-            <button className="rounded-lg bg-blue-400 p-2 px-6 md:px-8 lg:px-10 text-center text-lg font-bold text-slate-800 shadow-md hover:bg-blue-500 dark:bg-blue-300 dark:shadow-lg dark:hover:bg-blue-400">
+            <button className="rounded-lg bg-blue-400 p-2 px-6 text-center text-lg font-bold text-slate-800 shadow-md hover:bg-blue-500 dark:bg-blue-300 dark:shadow-lg dark:hover:bg-blue-400 md:px-8 lg:px-10">
               Projects
             </button>
           </Link>
           <Link href="https://github.com/axisiscool">
-            <button className="px-6 rounded-lg bg-blue-400 p-2 md:px-8 lg:px-10 text-center text-lg font-bold text-slate-800 shadow-md hover:bg-blue-500 dark:bg-blue-300 dark:shadow-lg dark:hover:bg-blue-400">
+            <button className="rounded-lg bg-blue-400 p-2 px-6 text-center text-lg font-bold text-slate-800 shadow-md hover:bg-blue-500 dark:bg-blue-300 dark:shadow-lg dark:hover:bg-blue-400 md:px-8 lg:px-10">
               Github
             </button>
           </Link>
           <Link href="https://discord.com/users/461750180388274180">
-            <button className="px-6 md:px-8 lg:px-10 rounded-lg bg-blue-400 p-2 text-center text-lg font-bold text-slate-800 shadow-md hover:bg-blue-500 dark:bg-blue-300 dark:shadow-lg dark:hover:bg-blue-400">
+            <button className="rounded-lg bg-blue-400 p-2 px-6 text-center text-lg font-bold text-slate-800 shadow-md hover:bg-blue-500 dark:bg-blue-300 dark:shadow-lg dark:hover:bg-blue-400 md:px-8 lg:px-10">
               Discord
             </button>
           </Link>
